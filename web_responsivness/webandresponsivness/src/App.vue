b5c65e1e4eb63341713667c1d433f790e032f840
Nathnael Desta <destanathnaelataro@gmail.com>
2024-07-09 04:13:28 +0300

    do poster

---
+++ web_responsivness/webandresponsivness/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+	<event-poster
+		:events="events"
+		:current-event="currentEvent"
+	>
+	</event-poster>
+</template>
+<script>
+	import EventPoster from './components/EventPoster.vue';
+	export default {
+		components: {
+			EventPoster,
+		},
+		data() {
+			return {
+				events: [],
+				currentEvent: 0,
+			};
+		},
+		created() {
+			this.getEvents();
+		},
+		methods: {
+			async getEvents() {
+				let res = await fetch('events.json');
+				let data = await res.json();
+
+				this.events = data;
+				console.log('the events are ', this.events[0]);
+			},
+		},
+	};
+</script>
